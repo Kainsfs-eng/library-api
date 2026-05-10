1154917d75b6803bb55060bcfb728e6d6dd53f92
Kingh <mnbvcxzak.ru@gmail.com>
2026-05-10 19:49:38 +0300

    create book model

--- book/models.py
+++ book/models.py
@@ -1,5 +1,16 @@
-from django.forms import models
-
+from django.db import models
 
 class Book(models.Model):
-    pass
+    class CoverChoices(models.TextChoices):
+        HARD = "Hard", "Hardcover"
+        SOFT = "SOFT", "Softcover"
+
+    author = models.CharField(max_length=100)
+    year_of_the_book = models.IntegerField()
+    title = models.CharField(max_length=255)
+
+    cover = models.CharField(
+        max_length=4,
+        choices=CoverChoices.choices,
+        default=CoverChoices.HARD
+        )
